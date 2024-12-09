a267da61d59cfd65e0bf4b646e42230479522c4a
IeroLys <64707387+IeroLys@users.noreply.github.com>
2024-12-09 23:45:39 +0300

    Update Task4.java

--- lab 9/Task4.java
+++ lab 9/Task4.java
@@ -5,39 +5,39 @@ import java.util.Scanner;
 public class Task4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the password: ");
+        System.out.println("Введите пароль: ");
         String password = scanner.nextLine();
         
-         // if the input is empty
+         // пустой ввод
         if (password.isEmpty()) {
-            System.out.println("You did not enter anything. Exiting the program.");
+            System.out.println("Пустой ввод.");
             return;
         }
         
         boolean hasUpper = password.matches(".*[A-Z].*"); // хотя бы 1 заглавная
         boolean hasLower = password.matches(".*[a-z].*");
-        boolean hasSpecial = password.matches(".*[#@\\$%\\^&*<>].*");
+        boolean hasSpecial = password.matches(".*[#@\\$%\\^&*<>].*"); // чтоб были такие символы
         boolean hasPunctuation = password.matches(".*[.,;:'\"!?-].*");
         boolean lengthValid = password.length() >= 8 && password.length() <= 12;
         
         if (hasUpper && hasLower && hasSpecial && hasPunctuation && lengthValid) {
-            System.out.println("Password is good.");
+            System.out.println("Хороший пароль.");
         } else {
-            System.out.println("Password is not good because it lacks:");
+            System.out.println("Не очень, отсутствует вот это:");
             if (!hasUpper) {
-                System.out.println("- Uppercase letters");
+                System.out.println("- Заглавные буквы");
             }
             if (!hasLower) {
-                System.out.println("- Lowercase letters");
+                System.out.println("- Маленькие буквы");
             }
             if (!hasSpecial) {
-                System.out.println("- Special symbols (#, @, $, %, ^, &, *, <, >)");
+                System.out.println("- Спец. символы (#, @, $, %, ^, &, *, <, >)");
             }
             if (!hasPunctuation) {
-                System.out.println("- Punctuation marks (.,;:'\"!?-)");
+                System.out.println("- Знаки препинания (.,;:'\"!?-)");
             }
             if (!lengthValid) {
-                System.out.println("- Proper length (8 to 12 characters)");
+                System.out.println("- Необходимая длина (8 - 12 символов)");
             }
         }
     }
