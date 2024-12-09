647f7667ef7304f505af6156f447506e39e89082
IeroLys <64707387+IeroLys@users.noreply.github.com>
2024-12-09 12:48:11 +0300

    Update indiv2

---
+++ indiv2/Course.java
@@ -0,0 +1,73 @@
+public class Course {
+    private String id;
+    private String name;
+    private String url;
+    private boolean isFree;
+    private double cost;
+    private int numberOfStudents;
+    private int numberOfReviews;
+    private int numberOfLectures;
+    private String difficulty;
+    private int durationMonths;
+
+    public Course(String id, String name, String url, boolean isFree, double cost,
+                  int numberOfStudents, int numberOfReviews, int numberOfLectures,
+                  String difficulty, int durationMonths) {
+        this.id = id;
+        this.name = name;
+        this.url = url;
+        this.isFree = isFree;
+        this.cost = cost;
+        this.numberOfStudents = numberOfStudents;
+        this.numberOfReviews = numberOfReviews;
+        this.numberOfLectures = numberOfLectures;
+        this.difficulty = difficulty;
+        this.durationMonths = durationMonths;
+    }
+
+    // Getters and setters omitted for brevity
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public boolean isFree() {
+        return isFree;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public int getNumberOfLectures() {
+        return numberOfLectures;
+    }
+
+    public int getDurationMonths() {
+        return durationMonths;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", url='" + url + '\'' +
+                ", isFree=" + isFree +
+                ", cost=" + cost +
+                ", numberOfStudents=" + numberOfStudents +
+                ", numberOfReviews=" + numberOfReviews +
+                ", numberOfLectures=" + numberOfLectures +
+                ", difficulty='" + difficulty + '\'' +
+                ", durationMonths=" + durationMonths +
+                '}';
+    }
+}
