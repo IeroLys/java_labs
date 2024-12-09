358f709e0789238c7c6611ef9d78879310becadc
IeroLys <64707387+IeroLys@users.noreply.github.com>
2024-12-09 22:30:00 +0300

    Update Student.java

--- lab 5 - Наследование/Student.java
+++ lab 5 - Наследование/Student.java
@@ -1,37 +1,60 @@
-public abstract class Student {
-    private String name;
-    private String gender;
-    private int age;
+import java.util.ArrayList;
+import java.util.List;
+
+// класс Студент по заданию
+
+public class Student extends Pupil {
+    private List<Integer> examGrades;
+    private List<Integer> courseworkGrades;
 
     public Student(String name, String gender, int age) {
-        this.name = name;
-        this.gender = gender;
-        this.age = age;
+        super(name, gender, age);
+        this.examGrades = new ArrayList<>();
+        this.courseworkGrades = new ArrayList<>();
     }
 
-    public String getName() {
-        return name;
+    public void addExamGrade(int grade) {
+        examGrades.add(grade);
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void addCourseworkGrade(int grade) {
+        courseworkGrades.add(grade);
     }
 
-    public String getGender() {
-        return gender;
+    public List<Integer> getCourseworkGrades() {
+        return courseworkGrades;
     }
 
-    public void setGender(String gender) {
-        this.gender = gender;
+    // ср.балл не ниже 4.75 и оценка 5 за курсовые
+    @Override
+    public boolean isEligibleForScholarship() {
+        if (examGrades.isEmpty()) {
+            return false;
+        }
+        double averageExamScore = examGrades.stream().mapToInt(Integer::intValue).average().orElse(0.0);
+        if (averageExamScore < 4.75) {
+            return false;
+        }
+        if (courseworkGrades.isEmpty()) {
+            return false;
+        }
+        for (int grade : courseworkGrades) {
+            if (grade != 5) {
+                return false;
+            }
+        }
+        return true;
     }
 
-    public int getAge() {
-        return age;
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + getName() + '\'' +
+                ", gender='" + getGender() + '\'' +
+                ", age=" + getAge() +
+                ", examGrades=" + examGrades +
+                ", courseworkGrades=" + courseworkGrades +
+                '}';
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public abstract boolean isEligibleForScholarship();
 }
