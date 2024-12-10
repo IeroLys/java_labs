8991f42ab3983232af31f8ec26be64918f79653f
IeroLys <64707387+IeroLys@users.noreply.github.com>
2024-12-10 11:45:02 +0300

    Update lab 6 Интерфейсы

---
+++ lab 6 Интерфейсы/SchoolStudent.java
@@ -0,0 +1,77 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SchoolStudent extends Pupil {
+    private HashMap<String, Integer> subjectGrades;
+    private List<OlympiadParticipation> olympiadParticipations;
+    private int schoolNumber;
+
+    public SchoolStudent(String name, String surname, String gender, int age, int schoolNumber) throws InvalidDataException {
+        super(name, surname, gender, age);
+        this.subjectGrades = new HashMap<>();
+        this.olympiadParticipations = new ArrayList<>();
+        if (schoolNumber <= 0) {
+            throw new InvalidDataException("School number must be positive.");
+        }
+        this.schoolNumber = schoolNumber;
+    }
+
+    public void addSubjectGrade(String subject, int grade) throws InvalidDataException {
+        if (grade < 2 || grade > 5) {
+            throw new InvalidDataException("Grade must be between 2 and 5.");
+        }
+        subjectGrades.put(subject, grade);
+    }
+
+    public void addOlympiadParticipation(OlympiadParticipation participation) {
+        olympiadParticipations.add(participation);
+    }
+
+    public int getSchoolNumber() {
+        return schoolNumber;
+    }
+
+    @Override
+    public boolean isEligibleForScholarship() {
+        String[] requiredSubjects = {"math", "russian", "history", "english"};
+        for (String subject : requiredSubjects) {
+            if (!subjectGrades.containsKey(subject) || subjectGrades.get(subject) != 5) {
+                return false;
+            }
+        }
+        for (int grade : subjectGrades.values()) {
+            if (grade < 4) {
+                return false;
+            }
+        }
+        for (OlympiadParticipation participation : olympiadParticipations) {
+            if (participation.getLevel() == OlympiadLevel.REGIONAL) {
+                return true;
+            } else if (participation.getLevel() == OlympiadLevel.SCHOOL && participation.getPlace() == 1) {
+                return true;
+            } else if (participation.getLevel() == OlympiadLevel.CITY && participation.getPlace() <= 3) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "SchoolStudent{" +
+                "name='" + getName() + '\'' +
+                ", surname='" + getSurname() + '\'' +
+                ", gender='" + getGender() + '\'' +
+                ", age=" + getAge() +
+                ", schoolNumber=" + schoolNumber +
+                ", subjectGrades=" + subjectGrades +
+                ", olympiadParticipations=" + olympiadParticipations +
+                '}';
+    }
+
+    @Override
+    public double getAcademicPerformanceRating() {
+        return subjectGrades.values().stream().mapToInt(Integer::intValue).average().orElse(0.0);
+    }
+}
