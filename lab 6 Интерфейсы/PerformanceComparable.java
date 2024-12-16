be3a19858f334686ecd4f5c1463bcdb5c8743a54
IeroLys <64707387+IeroLys@users.noreply.github.com>
2024-12-16 21:36:11 +0300

    Update PerformanceComparable.java

--- lab 6 Интерфейсы/PerformanceComparable.java
+++ lab 6 Интерфейсы/PerformanceComparable.java
@@ -1,3 +1,5 @@
+//для получения рейтинга академической успеваемости объекта класса.
+// будем сравнивать объекты
 public interface PerformanceComparable {
     double getAcademicPerformanceRating();
 }
