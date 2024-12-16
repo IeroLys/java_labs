7a51b6568c7e02bb703832f0a6d4d7b6764042ee
IeroLys <64707387+IeroLys@users.noreply.github.com>
2024-12-16 15:35:59 +0300

    Update InvalidDataException.java

--- lab 6 Интерфейсы/InvalidDataException.java
+++ lab 6 Интерфейсы/InvalidDataException.java
@@ -1,5 +1,7 @@
+// пользовательское исключение
 public class InvalidDataException extends Exception {
     public InvalidDataException(String message) {
+//вызываем конструктор родительского класса, передаём ошибку
         super(message);
     }
 }
