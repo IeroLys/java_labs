f04fd746c120581b108daf8a6d2cf2803d10782b
IeroLys <64707387+IeroLys@users.noreply.github.com>
2024-12-17 08:50:45 +0300

    Update SentenceAnalysis.java

--- Lab 8/SentenceAnalysis.java
+++ Lab 8/SentenceAnalysis.java
@@ -12,19 +12,21 @@ public class SentenceAnalysis {
         this.sentence = sentence;
     }
 
-// лямбда
+// лямбда, считаем слова на "es"
     public int countWordsEndingWithEs() {
         return (int) Arrays.stream(sentence.split("[ ,]+"))
                 .filter(word -> word.endsWith("es"))
                 .count();
     }
 
+// сортируем по длине слов
     public List<String> getWordsSortedByLength() {
         List<String> words = new ArrayList<>(Arrays.asList(sentence.split("[ ,]+")));
         words.sort(Comparator.comparingInt(String::length));
         return words;
     }
 
+// считаем сумму целых чисел в предложении
     public int sumOfIntegersInSentence() {
         int sum = 0;
         Pattern pattern = Pattern.compile("-?\\d+");
