b1d1995f8a76c22c47a6eff621605fa0040a66aa
IeroLys <64707387+IeroLys@users.noreply.github.com>
2024-12-17 08:51:41 +0300

    Update Main.java

--- Lab 8/Main.java
+++ Lab 8/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        // Problem 1
+        // Задача 1
         TemperatureAnalysis temperatureAnalysis = new TemperatureAnalysis();
         System.out.println("Количество дней с отрицательной температурой: " + temperatureAnalysis.countNegativeDays());
         System.out.println("Были ли дни с температурой выше 10 градусов? " + temperatureAnalysis.hasTemperaturesAboveTen());
         System.out.println("Максимальная температура в первую неделю марта: " + temperatureAnalysis.getMaxTemperatureFirstWeek());
         System.out.println("Средняя температура за месяц: " + temperatureAnalysis.calculateAverageTemperature());
 
-        // Problem 2
+        // Задача 2
         String sentence = "They used 233 features including 227 stylometric features and six novel social " +
                 "network-specific features like character-based ones numbers of alphabets, uppercase characters, " +
                 "special characters, word-based ones the total number of words, average word length, the number of " +
